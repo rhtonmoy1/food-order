e60314e39598c52f0017e3545f1db5b37e16e591
Rafiul Hasan Tonmoy <rhtonmoyhd@gmail.com>
2023-05-02 13:42:03 +0600

    db change

--- admin/inc/config.php
+++ admin/inc/config.php
@@ -9,7 +9,7 @@ date_default_timezone_set('America/Los_Angeles');
 $dbhost = 'localhost';
 
 // Database Name
-$dbname = 'ecommerceweb';
+$dbname = 'food-order';
 
 // Database Username
 $dbuser = 'root';
