4dd2acf19a3a34730c6594731f303a33e862bdee
Rafiul Hasan Tonmoy <rhtonmoyhd@gmail.com>
2023-05-16 09:48:16 +0600

    fix product add

--- admin/product-add.php
+++ admin/product-add.php
@@ -1,105 +1,102 @@
 <?php require_once('header.php'); ?>
 
 <?php
-if(isset($_POST['form1'])) {
+if (isset($_POST['form1'])) {
 	$valid = 1;
 
-    if(empty($_POST['tcat_id'])) {
-        $valid = 0;
-        $error_message .= "You must have to select a top level category<br>";
-    }
+	if (empty($_POST['tcat_id'])) {
+		$valid = 0;
+		$error_message .= "You must have to select a top level category<br>";
+	}
 
-    if(empty($_POST['mcat_id'])) {
-        $valid = 0;
-        $error_message .= "You must have to select a mid level category<br>";
-    }
+	if (empty($_POST['mcat_id'])) {
+		$valid = 0;
+		$error_message .= "You must have to select a mid level category<br>";
+	}
 
-    if(empty($_POST['ecat_id'])) {
-        $valid = 0;
-        $error_message .= "You must have to select an end level category<br>";
-    }
+	if (empty($_POST['ecat_id'])) {
+		$valid = 0;
+		$error_message .= "You must have to select an end level category<br>";
+	}
 
-    if(empty($_POST['p_name'])) {
-        $valid = 0;
-        $error_message .= "Product name can not be empty<br>";
-    }
+	if (empty($_POST['p_name'])) {
+		$valid = 0;
+		$error_message .= "Product name can not be empty<br>";
+	}
 
-    // if(empty($_POST['p_current_price'])) {
-    //     $valid = 0;
-    //     $error_message .= "Current Price can not be empty<br>";
-    // }
+	// if(empty($_POST['p_current_price'])) {
+	//     $valid = 0;
+	//     $error_message .= "Current Price can not be empty<br>";
+	// }
 
-    // if(empty($_POST['p_qty'])) {
-    //     $valid = 0;
-    //     $error_message .= "Quantity can not be empty<br>";
-    // }
+	// if(empty($_POST['p_qty'])) {
+	//     $valid = 0;
+	//     $error_message .= "Quantity can not be empty<br>";
+	// }
 
-    $path = $_FILES['p_featured_photo']['name'];
-    $path_tmp = $_FILES['p_featured_photo']['tmp_name'];
+	$path = $_FILES['p_featured_photo']['name'];
+	$path_tmp = $_FILES['p_featured_photo']['tmp_name'];
 
-    if($path!='') {
-        $ext = pathinfo( $path, PATHINFO_EXTENSION );
-        $file_name = basename( $path, '.' . $ext );
-        if( $ext!='jpg' && $ext!='png' && $ext!='jpeg' && $ext!='gif' ) {
-            $valid = 0;
-            $error_message .= 'You must have to upload jpg, jpeg, gif or png file<br>';
-        }
-    } else {
-    	$valid = 0;
-        $error_message .= 'You must have to select a featured photo<br>';
-    }
+	if ($path != '') {
+		$ext = pathinfo($path, PATHINFO_EXTENSION);
+		$file_name = basename($path, '.' . $ext);
+		if ($ext != 'jpg' && $ext != 'png' && $ext != 'jpeg' && $ext != 'gif') {
+			$valid = 0;
+			$error_message .= 'You must have to upload jpg, jpeg, gif or png file<br>';
+		}
+	} else {
+		$valid = 0;
+		$error_message .= 'You must have to select a featured photo<br>';
+	}
 
 
-    if($valid == 1) {
+	if ($valid == 1) {
 
-    	$statement = $pdo->prepare("SHOW TABLE STATUS LIKE 'tbl_product'");
+		$statement = $pdo->prepare("SHOW TABLE STATUS LIKE 'tbl_product'");
 		$statement->execute();
 		$result = $statement->fetchAll();
-		foreach($result as $row) {
-			$ai_id=$row[10];
+		foreach ($result as $row) {
+			$ai_id = $row[10];
 		}
 
-    	if( isset($_FILES['photo']["name"]) && isset($_FILES['photo']["tmp_name"]) )
-        {
-        	$photo = array();
-            $photo = $_FILES['photo']["name"];
-            $photo = array_values(array_filter($photo));
+		if (isset($_FILES['photo']["name"]) && isset($_FILES['photo']["tmp_name"])) {
+			$photo = array();
+			$photo = $_FILES['photo']["name"];
+			$photo = array_values(array_filter($photo));
 
-        	$photo_temp = array();
-            $photo_temp = $_FILES['photo']["tmp_name"];
-            $photo_temp = array_values(array_filter($photo_temp));
+			$photo_temp = array();
+			$photo_temp = $_FILES['photo']["tmp_name"];
+			$photo_temp = array_values(array_filter($photo_temp));
 
-        	$statement = $pdo->prepare("SHOW TABLE STATUS LIKE 'tbl_product_photo'");
+			$statement = $pdo->prepare("SHOW TABLE STATUS LIKE 'tbl_product_photo'");
 			$statement->execute();
 			$result = $statement->fetchAll();
-			foreach($result as $row) {
-				$next_id1=$row[10];
+			foreach ($result as $row) {
+				$next_id1 = $row[10];
 			}
 			$z = $next_id1;
 
-            $m=0;
-            for($i=0;$i<count($photo);$i++)
-            {
-                $my_ext1 = pathinfo( $photo[$i], PATHINFO_EXTENSION );
-		        if( $my_ext1=='jpg' || $my_ext1=='png' || $my_ext1=='jpeg' || $my_ext1=='gif' ) {
-		            $final_name1[$m] = $z.'.'.$my_ext1;
-                    move_uploaded_file($photo_temp[$i],"../assets/uploads/product_photos/".$final_name1[$m]);
-                    $m++;
-                    $z++;
-		        }
-            }
+			$m = 0;
+			for ($i = 0; $i < count($photo); $i++) {
+				$my_ext1 = pathinfo($photo[$i], PATHINFO_EXTENSION);
+				if ($my_ext1 == 'jpg' || $my_ext1 == 'png' || $my_ext1 == 'jpeg' || $my_ext1 == 'gif') {
+					$final_name1[$m] = $z . '.' . $my_ext1;
+					move_uploaded_file($photo_temp[$i], "../assets/uploads/product_photos/" . $final_name1[$m]);
+					$m++;
+					$z++;
+				}
+			}
 
-            if(isset($final_name1)) {
-            	for($i=0;$i<count($final_name1);$i++)
-		        {
-		        	$statement = $pdo->prepare("INSERT INTO tbl_product_photo (photo,p_id) VALUES (?,?)");
-		        	$statement->execute(array($final_name1[$i],$ai_id));
-		        }
-            }            
-        }
+			if (isset($final_name1)) {
+				for ($i = 0; $i < count($final_name1); $i++) {
+					$statement = $pdo->prepare("INSERT INTO tbl_product_photo (photo,p_id) VALUES (?,?)");
+					$statement->execute(array($final_name1[$i], $ai_id));
+				}
+			}
+		}
 
-		$final_name = 'product-featured-'.$ai_id.'.'.$ext;
-        move_uploaded_file( $path_tmp, '../assets/uploads/'.$final_name );
+		$final_name = 'product-featured-' . $ai_id . '.' . $ext;
+		move_uploaded_file($path_tmp, '../assets/uploads/' . $final_name);
 
 		//Saving data into the main table tbl_product
 		$statement = $pdo->prepare("INSERT INTO tbl_product(
@@ -115,37 +112,39 @@ if(isset($_POST['form1'])) {
 										p_total_view,
 										p_is_featured,
 										p_is_active,
-										ecat_id
+										ecat_id,
+										is_vage
 									) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 		$statement->execute(array(
-										$_POST['p_name'],
-										$_POST['p_old_price'],
-										$_POST['p_current_price'],
-										$_POST['p_qty'],
-										$final_name,
-										$_POST['p_description'],
-										$_POST['p_short_description'],
-										$_POST['p_feature'],
-										$_POST['p_condition'],
-										0,
-										$_POST['p_is_featured'],
-										$_POST['p_is_active'],
-										$_POST['ecat_id']
-									));
+			$_POST['p_name'],
+			$_POST['p_old_price'],
+			$_POST['p_current_price'],
+			$_POST['p_qty'],
+			$final_name,
+			$_POST['p_description'],
+			$_POST['p_short_description'],
+			$_POST['p_feature'],
+			$_POST['p_condition'],
+			0,
+			$_POST['p_is_featured'],
+			$_POST['p_is_active'],
+			$_POST['ecat_id'],
+			$_POST['is_vage']
+		));
 
-		
 
-        if(isset($_POST['size'])) {
-			foreach($_POST['size'] as $value) {
+
+		if (isset($_POST['size'])) {
+			foreach ($_POST['size'] as $value) {
 				$statement = $pdo->prepare("INSERT INTO tbl_product_size (size_id,p_id) VALUES (?,?)");
-				$statement->execute(array($value,$ai_id));
+				$statement->execute(array($value, $ai_id));
 			}
 		}
 
-		
-	
-    	$success_message = 'Product is added successfully.';
-    }
+
+
+		$success_message = 'Product is added successfully.';
+	}
 }
 ?>
 
@@ -164,20 +163,20 @@ if(isset($_POST['form1'])) {
 	<div class="row">
 		<div class="col-md-12">
 
-			<?php if($error_message): ?>
-			<div class="callout callout-danger">
-			
-			<p>
-			<?php echo $error_message; ?>
-			</p>
-			</div>
+			<?php if ($error_message) : ?>
+				<div class="callout callout-danger">
+
+					<p>
+						<?php echo $error_message; ?>
+					</p>
+				</div>
 			<?php endif; ?>
 
-			<?php if($success_message): ?>
-			<div class="callout callout-success">
-			
-			<p><?php echo $success_message; ?></p>
-			</div>
+			<?php if ($success_message) : ?>
+				<div class="callout callout-success">
+
+					<p><?php echo $success_message; ?></p>
+				</div>
 			<?php endif; ?>
 
 			<form class="form-horizontal" action="" method="post" enctype="multipart/form-data">
@@ -192,11 +191,11 @@ if(isset($_POST['form1'])) {
 									<?php
 									$statement = $pdo->prepare("SELECT * FROM tbl_top_category ORDER BY tcat_name ASC");
 									$statement->execute();
-									$result = $statement->fetchAll(PDO::FETCH_ASSOC);	
+									$result = $statement->fetchAll(PDO::FETCH_ASSOC);
 									foreach ($result as $row) {
-										?>
+									?>
 										<option value="<?php echo $row['tcat_id']; ?>"><?php echo $row['tcat_name']; ?></option>
-										<?php
+									<?php
 									}
 									?>
 								</select>
@@ -223,7 +222,7 @@ if(isset($_POST['form1'])) {
 							<div class="col-sm-4">
 								<input type="text" name="p_name" class="form-control">
 							</div>
-						</div>	
+						</div>
 						<div class="form-group">
 							<label for="" class="col-sm-3 control-label">Old Price <br><span style="font-size:10px;font-weight:normal;">(In USD)</span></label>
 							<div class="col-sm-4">
@@ -235,7 +234,7 @@ if(isset($_POST['form1'])) {
 							<div class="col-sm-4">
 								<input type="text" name="p_current_price" class="form-control">
 							</div>
-						</div>	
+						</div>
 						<div class="form-group">
 							<label for="" class="col-sm-3 control-label">Quantity <span>*</span></label>
 							<div class="col-sm-4">
@@ -249,17 +248,17 @@ if(isset($_POST['form1'])) {
 									<?php
 									$statement = $pdo->prepare("SELECT * FROM tbl_size ORDER BY size_id ASC");
 									$statement->execute();
-									$result = $statement->fetchAll(PDO::FETCH_ASSOC);			
+									$result = $statement->fetchAll(PDO::FETCH_ASSOC);
 									foreach ($result as $row) {
-										?>
+									?>
 										<option value="<?php echo $row['size_id']; ?>"><?php echo $row['size_name']; ?></option>
-										<?php
+									<?php
 									}
 									?>
 								</select>
 							</div>
 						</div>
-				
+
 						<div class="form-group">
 							<label for="" class="col-sm-3 control-label">Featured Photo <span>*</span></label>
 							<div class="col-sm-4" style="padding-top:4px;">
@@ -270,21 +269,21 @@ if(isset($_POST['form1'])) {
 							<label for="" class="col-sm-3 control-label">Other Photos</label>
 							<div class="col-sm-4" style="padding-top:4px;">
 								<table id="ProductTable" style="width:100%;">
-			                        <tbody>
-			                            <tr>
-			                                <td>
-			                                    <div class="upload-btn">
-			                                        <input type="file" name="photo[]" style="margin-bottom:5px;">
-			                                    </div>
-			                                </td>
-			                                <td style="width:28px;"><a href="javascript:void()" class="Delete btn btn-danger btn-xs">X</a></td>
-			                            </tr>
-			                        </tbody>
-			                    </table>
+									<tbody>
+										<tr>
+											<td>
+												<div class="upload-btn">
+													<input type="file" name="photo[]" style="margin-bottom:5px;">
+												</div>
+											</td>
+											<td style="width:28px;"><a href="javascript:void()" class="Delete btn btn-danger btn-xs">X</a></td>
+										</tr>
+									</tbody>
+								</table>
 							</div>
 							<div class="col-sm-2">
-			                    <input type="button" id="btnAddNew" value="Add Item" style="margin-top: 5px;margin-bottom:10px;border:0;color: #fff;font-size: 14px;border-radius:3px;" class="btn btn-warning btn-xs">
-			                </div>
+								<input type="button" id="btnAddNew" value="Add Item" style="margin-top: 5px;margin-bottom:10px;border:0;color: #fff;font-size: 14px;border-radius:3px;" class="btn btn-warning btn-xs">
+							</div>
 						</div>
 						<div class="form-group">
 							<label for="" class="col-sm-3 control-label">Description</label>
@@ -310,14 +309,14 @@ if(isset($_POST['form1'])) {
 								<textarea name="p_condition" class="form-control" cols="30" rows="10" id="editor4"></textarea>
 							</div>
 						</div>
-						
+
 						<div class="form-group">
 							<label for="" class="col-sm-3 control-label">Is Featured?</label>
 							<div class="col-sm-8">
 								<select name="p_is_featured" class="form-control" style="width:auto;">
 									<option value="0">No</option>
 									<option value="1">Yes</option>
-								</select> 
+								</select>
 							</div>
 						</div>
 						<div class="form-group">
@@ -326,7 +325,16 @@ if(isset($_POST['form1'])) {
 								<select name="p_is_active" class="form-control" style="width:auto;">
 									<option value="0">No</option>
 									<option value="1">Yes</option>
-								</select> 
+								</select>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="" class="col-sm-3 control-label">IS_Vage<span>*</span></label>
+							<div class="col-sm-4">
+								<select name="is_vage" class="form-control" style="width:auto;">
+									<option value="0">No</option>
+									<option value="1">Yes</option>
+								</select>
 							</div>
 						</div>
 						<div class="form-group">
